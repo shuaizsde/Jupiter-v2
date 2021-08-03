e4ba47fd8c2e6b4d34935e416c4d9c0fdbfb1bc6
simon zhang <zhang86036055@gmail.com>
2021-08-02 20:11:56 -0700

    update

--- jupiter/src/main/java/com/laioffer/jupiter/servlet/LogoutServlet.java
+++ jupiter/src/main/java/com/laioffer/jupiter/servlet/LogoutServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "LogoutServlet", value = "/LogoutServlet")
+@WebServlet(name = "LogoutServlet", value = "/logout")
 public class LogoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Destroy the session since the user is logged out.
